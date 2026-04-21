aee12d3d24b4c333be8fba35f69783a202b342db
Konstantin Balandin <kostabalandin169@gmail.com>
2026-04-21 15:52:30 +0700

    edit header files

--- include/repo.h
+++ include/repo.h
@@ -3,10 +3,8 @@
 
 #include "minigit.h"
 
-// Инициализирует новый репозиторий на диске (низкоуровневая функция)
 MinigitStatus init_repo_disk(void);
 
-// Проверяет, существует ли репозиторий в текущей директории
 int repo_exists(void);
 
 #endif
--- include/tree.h
+++ include/tree.h
@@ -4,7 +4,6 @@
 #include "minigit.h"
 #include <stdbool.h>
 
-// Узел дерева (файл или папка)
 typedef struct TreeNode {
     char *name;             // Имя файла или папки
     char *hash;             // Хеш содержимого (для файла) или поддерева
@@ -14,28 +13,20 @@ typedef struct TreeNode {
     int ref_count;          // Счетчик ссылок для structural sharing
 } TreeNode;
 
-// Создать пустое дерево
 TreeNode* tree_create(void);
 
-// Добавить/обновить файл в дереве (возвращает НОВОЕ дерево)
 TreeNode* tree_add_file(TreeNode *old_tree, const char *path, const char *hash);
 
-// Удалить файл из дерева (возвращает НОВОЕ дерево)
 TreeNode* tree_remove_file(TreeNode *old_tree, const char *path);
 
-// Получить хеш файла по пути
 const char* tree_get_file_hash(TreeNode *tree, const char *path);
 
-// Проверить существование файла
 bool tree_file_exists(TreeNode *tree, const char *path);
 
-// Вывести список файлов
 void tree_print_files(TreeNode *tree, int indent);
 
-// Освободить память (с учетом ref_count)
 void tree_free(TreeNode *tree);
 
-// Клонировать узел (неглубокое копирование для structural sharing)
 TreeNode* tree_clone_node(TreeNode *node);
 
 #endif
